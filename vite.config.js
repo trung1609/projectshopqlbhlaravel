c57bbc6371e952fff867a041a05c184060dce4c5
trung1609 <vmt162005@gmail.com>
2025-02-26 21:05:20 +0700

    bbb

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,17 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+    // plugins: [
+    //     laravel({
+    //         input: ['resources/css/app.css', 'resources/js/app.js'],
+    //         refresh: true,
+    //     }),
+    // ],
+    export default defineConfig({
+        server: {
+          host: '0.0.0.0',
+          port: 4173 // Hoặc cổng Render cấp
+        }
+      });
+      
 });
